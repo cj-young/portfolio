7a3d662b5d53d391735565171269fd2fd4b5e158
CJ Young <chrjyoung@gmail.com>
2024-02-05 23:45:02 -0500

    💄 Shorten max width of about section text

--- src/pages/home/components/AboutSection/index.tsx
+++ src/pages/home/components/AboutSection/index.tsx
@@ -1,8 +1,8 @@
 export default function AboutSection() {
   return (
     <section className="relative flex h-screen w-full flex-col items-center justify-center">
-      <h2 className="text-purple text-subtitle font-bold">About Me</h2>
-      <p className="mt-4 max-w-[37rem] text-center text-base text-gray-400">
+      <h2 className="text-subtitle font-bold text-purple">About Me</h2>
+      <p className="mt-4 max-w-[40ch] text-center text-base text-gray-400">
         Lorem ipsum dolor sit amet consectetur. Amet scelerisque elementum augue
         lobortis tortor augue ut aliquam facilisis. Non amet arcu risus non
         tempor. A sit maecenas leo pellentesque. At mattis cras congue a
