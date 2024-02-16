ed8a5b0e4d96963b4cbe149375c967672f5a99bb
CJ Young <chrjyoung@gmail.com>
2024-02-15 22:33:38 -0500

    🐛 Fix translation properties not updating on window resize

--- src/pages/home/components/SkillsSection/index.tsx
+++ src/pages/home/components/SkillsSection/index.tsx
@@ -11,17 +11,22 @@ function getSkillNodeTranslations(cicrleIndex: number) {
   const angle =
     90 + SKILL_NODE_OFFSET_ANGLE + ANGLE_BETWEEN_SKILL_NODES * cicrleIndex;
   return [
-    `calc(-50% + cos(${angle}deg) * 11rem)`,
-    `calc(-50% - sin(${angle}deg) * 11rem)`,
+    `calc(cos(${angle}deg) * 11rem)`,
+    `calc(-1 * sin(${angle}deg) * 11rem)`,
   ];
 }
 
 const skillImages = Array.from({ length: 9 }, () => null);
 
 export default function SkillsSection() {
-  const { parentRef, scrollTargetRef } = useStaggeredFadeIn<HTMLDivElement>({
-    duration: 0.5,
-  });
+  const { parentRef: mobileParentRef, scrollTargetRef: mobileScrollTargetRef } =
+    useStaggeredFadeIn<HTMLDivElement>({
+      duration: 0.5,
+    });
+  const { parentRef: largeParentRef, scrollTargetRef: largeScrollTargetRef } =
+    useStaggeredFadeIn<HTMLDivElement>({
+      duration: 0.5,
+    });
   const { parentRef: grandparentRef } = useStaggeredFadeIn<HTMLDivElement>({
     attributeName: "data-animate-grandparent",
   });
@@ -30,18 +35,36 @@ export default function SkillsSection() {
     <section className="relative flex h-screen w-full items-center justify-center">
       <div
         className="flex flex-col items-center"
-        ref={useMergedRef(grandparentRef, scrollTargetRef)}
+        ref={useMergedRef(
+          grandparentRef,
+          mobileScrollTargetRef,
+          largeScrollTargetRef,
+        )}
       >
         <h2 className="text-subtitle font-bold text-purple">My Skills</h2>
         <div
-          className="mt-4 grid grid-cols-3 gap-4 md:absolute md:left-1/2 md:top-1/2 md:mt-0 md:block md:-translate-x-1/2 md:-translate-y-1/2"
-          ref={parentRef}
+          className="mt-4 grid grid-cols-3 gap-4 md:hidden"
+          ref={mobileParentRef}
           data-animate-grandparent="false"
         >
           {skillImages.map((_image, i) => (
             <div
               className={
-                "left-0 top-0 h-20 w-20 rounded-[1000vmax] bg-white shadow-[0_0_16px_4px_rgba(0,0,0,15%)] md:absolute md:translate-x-[var(--circle-translate-x)] md:translate-y-[var(--circle-translate-y)]"
+                "h-20 w-20 rounded-[1000vmax] bg-white shadow-[0_0_16px_4px_rgba(0,0,0,15%)]"
+              }
+              key={i}
+            ></div>
+          ))}
+        </div>
+        <div
+          className="absolute left-1/2 top-1/2 mt-0 hidden -translate-x-1/2 -translate-y-1/2 grid-cols-3 gap-4 md:block"
+          ref={largeParentRef}
+          data-animate-grandparent="false"
+        >
+          {skillImages.map((_image, i) => (
+            <div
+              className={
+                "absolute left-[var(--circle-translate-x)] top-[var(--circle-translate-y)] h-20 w-20 -translate-x-1/2 -translate-y-1/2 rounded-[1000vmax] bg-white shadow-[0_0_16px_4px_rgba(0,0,0,15%)]"
               }
               key={i}
               style={
