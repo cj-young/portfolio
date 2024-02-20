2d1ad6adfe2c181c547db3b50cd661a5c91368b0
CJ Young <chrjyoung@gmail.com>
2024-02-19 22:31:19 -0500

    💫 Add scroll parallax to skills section logos

--- src/pages/home/components/mesh-containers/SkillSectionLogos/index.tsx
+++ src/pages/home/components/mesh-containers/SkillSectionLogos/index.tsx
@@ -1,3 +1,6 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { Group } from "three";
 import {
   cssPositions,
   gitPositions,
@@ -11,9 +14,24 @@ import {
 } from "./logo-positions";
 import LogoWrapper from "./logos/LogoWrapper";
 
+const PAGE_INDEX = 2;
+const LOGOS_SCROLL_DAMPENING = 75;
+
 export default function SkillSectionLogos() {
+  const groupRef = useRef<Group>(null);
+
+  useFrame(() => {
+    const group = groupRef.current;
+    if (!group) return;
+
+    const pixelsScrolledFromSection =
+      PAGE_INDEX * window.innerHeight - window.scrollY;
+
+    group.position.y = pixelsScrolledFromSection / -LOGOS_SCROLL_DAMPENING;
+  });
+
   return (
-    <group>
+    <group ref={groupRef}>
       {jsPositions.map((position, i) => (
         <LogoWrapper position={position} skillId="js" key={`js${i}`} />
       ))}
