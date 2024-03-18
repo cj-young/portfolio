10aa5e0e29069411973f06f80817e0dcad8e1790
CJ Young <chrjyoung@gmail.com>
2024-03-17 20:12:28 -0400

    🎨 Add props to useFadeIn to further customization

--- src/hooks/useFadeIn.ts
+++ src/hooks/useFadeIn.ts
@@ -1,29 +1,44 @@
 import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+import gsap, { CSSPlugin } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { useRef } from "react";
-import { useScrollContext } from "../pages/home/contexts/ScrollContext";
 
-export default function useFadeIn() {
-  const elementRef = useRef(null);
-  const { scroller } = useScrollContext();
+type TConfig = {
+  duration?: number;
+  onAnimationFinished?(): void;
+  clearProps?: string;
+};
+/**
+ *
+ * @param {Object} containerRef The container that is being scrolled
+ * @param {Object} config The configuration object
+ * @param {number} config.duration The duration of each animation
+ * @param {string} config.clearProps The properties that are affected by animation and should be reset when finished
+ */
+export default function useFadeIn<T extends HTMLElement>(
+  containerRef: React.RefObject<HTMLDivElement>,
+  { duration = 0.5, onAnimationFinished, clearProps = "" }: TConfig = {},
+) {
+  const elementRef = useRef<T>(null);
 
   useGSAP(() => {
+    if (!elementRef.current) return;
     gsap.registerPlugin(ScrollTrigger);
-    gsap.fromTo(
-      elementRef.current,
-      { opacity: 0, translateY: "1rem" },
-      {
-        opacity: 1,
-        translateY: 0,
-        scrollTrigger: {
-          trigger: elementRef.current,
-          start: "top 66%",
-          scrub: false,
-          scroller: scroller.current,
-        },
+    gsap.registerPlugin(CSSPlugin);
+
+    gsap.from(elementRef.current, {
+      scrollTrigger: {
+        start: "top 75%",
+        scrub: false,
+        // markers: true,
+        scroller: containerRef.current,
       },
-    );
+      opacity: 0,
+      translateY: "+=1rem",
+      duration,
+      onComplete: onAnimationFinished,
+      clearProps,
+    });
   });
 
   return { elementRef };
