1a8dca2b4300a4e9c94ad2e012fb462beccb62b4
CJ Young <chrjyoung@gmail.com>
2024-03-22 00:45:00 -0400

    💫 Add fade in transition for contact form

--- src/pages/home/components/ContactSection/index.tsx
+++ src/pages/home/components/ContactSection/index.tsx
@@ -1,9 +1,17 @@
+import useStaggeredFadeIn from "@/src/hooks/useStaggeredFadeIn";
+import { useScrollContext } from "../../contexts/ScrollContext";
 import ContactForm from "./components/ContactForm";
 
 export default function ContactSection() {
+  const { scroller } = useScrollContext();
+  const { parentRef } = useStaggeredFadeIn(scroller);
+
   return (
-    <section className="flex min-h-screen w-full flex-col items-center justify-center">
-      <h2 className="text-purple text-subtitle font-bold">Contact</h2>
+    <section
+      className="flex min-h-screen w-full flex-col items-center justify-center"
+      ref={parentRef}
+    >
+      <h2 className="text-subtitle font-bold text-purple">Contact</h2>
       <ContactForm />
     </section>
   );
